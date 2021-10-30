1f98ccb160436cbf2106359da9911800002a59c2
Leandro Cabral <93451271+LeandroCabralAppSec@users.noreply.github.com>
2021-10-30 18:34:27 -0300

    Update Function.php

--- Function.php
+++ Function.php
@@ -1,7 +1,3 @@
-Para removermos estas informações, iremos criar uma função, será necessário alterar o functions.php de seu tema, coloque abaixo do primeiro cometário.
-To remove this information, we will create a function, it will be necessary to change the functions.php of your theme, put it below the first comment.
-
-
 /** # TIRAR A VERSÃO DO WORDPRESS # */
 // Remove WP Version From Styles 
 add_filter( 'style_loader_src', 'sdt_remove_ver_css_js', 9999 );
